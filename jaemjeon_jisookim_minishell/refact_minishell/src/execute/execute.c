40f397f424c5b7310666c880863951810a46d37b
Jisoo Kim <jisookim@c6r3s1.42seoul.kr>
2022-08-25 12:52:11 +0900

    feat : made execute folder

--- jaemjeon_jisookim_minishell/refact_minishell/src/execute/execute.c
+++ jaemjeon_jisookim_minishell/refact_minishell/src/execute/execute.c
@@ -1,22 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   env.c                                              :+:      :+:    :+:   */
+/*   execute.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jisookim <jisookim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/24 04:39:33 by jaemjeon          #+#    #+#             */
-/*   Updated: 2022/08/25 12:50:42 by jisookim         ###   ########.fr       */
+/*   Created: 2022/08/25 12:48:13 by jisookim          #+#    #+#             */
+/*   Updated: 2022/08/25 12:48:53 by jisookim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include "../../include/utils.h"
 
-void	built_in_env(t_envlst *env)
-{
-	while (env != NULL)
-	{
-		//printf("%s=%s\n", env->key, env->value);
-		env = env->next;
-	}
-}
