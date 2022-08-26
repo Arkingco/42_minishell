2f138152dd02626429ebcb24962aa71779e93ea2
Jisoo Kim <jisookim@c6r4s1.42seoul.kr>
2022-08-26 10:47:49 +0900

    feat : made directory, check if it compiles

--- jaemjeon_jisookim_minishell/refact_minishell/src/built_in/echo.c
+++ jaemjeon_jisookim_minishell/refact_minishell/src/built_in/echo.c
@@ -1,20 +1,51 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   execute.c                                          :+:      :+:    :+:   */
+/*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jisookim <jisookim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/25 12:48:13 by jisookim          #+#    #+#             */
-/*   Updated: 2022/08/26 10:07:17 by jisookim         ###   ########.fr       */
+/*   Created: 2022/08/26 09:21:09 by jisookim          #+#    #+#             */
+/*   Updated: 2022/08/26 10:19:12 by jisookim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-void	execute()
+int	is_echo_option(char *input);
+
+int	echo(char **inputs)
 {
-	//if 
-	//else
-	return ;
+	int	flag;
+	int	i;
+
+	i = 0;
+	while (inputs[i])
+	{
+		flag = is_echo_option(inputs[i]);
+		printf("%s", inputs[i]);
+		if (!flag)
+		{
+			printf("\n");
+			break ;
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	is_echo_option(char *input)
+{
+	int	i;
+
+	i = 1;
+	if (input[0] != '-')
+		return (0);
+	while (input[i])
+	{
+		if (input[i] != 'n')
+			return (0);
+		i++;
+	}
+	return (1);
 }
