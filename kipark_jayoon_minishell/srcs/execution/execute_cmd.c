a7563521394870895fcf80e1276eec3773dbec5f
“brixxt27” <brixxt27@gmail.com>
2022-09-01 15:55:32 +0900

    feat: branch 생성 후 실행 부분 설계

---
+++ kipark_jayoon_minishell/srcs/execution/execute_cmd.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execute_cmd.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jayoon <jayoon@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/01 11:41:32 by jayoon            #+#    #+#             */
+/*   Updated: 2022/09/01 14:11:29 by jayoon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parser.h"
+
+int	check_our_program()
+{}
+
+void	execute_cmd(t_parsing_list *l_parsing)
+{
+	/*
+	우리가 구현한 built in 프로그램 이름을 담고 있는 list 생성
+	t_
+
+	while l_parsing
+		if (check_our_program())
+			execute_implemented_program
+		else
+			execute_not_implemented_program
+		l_parsing = l_parsing->next
+	*/
+}
