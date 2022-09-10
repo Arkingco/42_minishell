0a7b4489eda21e21ed65a49af39fecd599fb63d3
Jaemin Jeong <jaemjeon@c1r6s6.42seoul.kr>
2022-09-10 18:00:40 +0900

    working

--- jaemjeon_jisookim_minishell/main/src/execute/execute.c
+++ jaemjeon_jisookim_minishell/main/src/execute/execute.c
@@ -6,7 +6,7 @@
 /*   By: jaemjeon <jaemjeon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 14:43:29 by jaemjeon          #+#    #+#             */
-/*   Updated: 2022/09/10 16:04:24 by jaemjeon         ###   ########.fr       */
+/*   Updated: 2022/09/10 18:00:29 by jaemjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,9 @@
 
 void	process_built_in(t_cmd *cmd, int cmd_type, t_envlst *env)
 {
-	void	(*built_in_func);
-	static const void (*built_in_func_board[BUILT_IN_COUNT])(t_cmd, t_envlst) = {
+	void	(*built_in_func)(t_cmd, t_envlst);
+	static const void (*built_in_func_board[BUILT_IN_COUNT])(t_cmd, t_envlst) =
+	{
 		[T_ECHO] = ft_echo,
 		[T_CD] = ft_cd,
 		[T_PWD] = ft_pwd,
@@ -23,9 +24,9 @@ void	process_built_in(t_cmd *cmd, int cmd_type, t_envlst *env)
 		[T_UNSET] = ft_unset,
 		[T_ENV] = ft_env,
 		[T_EXIT] = ft_exit
-	}
+	};
 
-	built_in_func = 
+	built_in_func_board[cmd_type];
 }
 
 void	process_single_cmd(t_cmd *cmd, t_envlst *env)
@@ -34,16 +35,14 @@ void	process_single_cmd(t_cmd *cmd, t_envlst *env)
 
 	cmd_type = get_cmd_type(cmd);
 	if (cmd_type == NOT_BUILT_IN)
-	{
-
-	}
+		run_in_single_child(cmd, env);
 	else
 		process_built_in(cmd, cmd_type, env);
 }
 
 void	process_multi_cmd(t_cmd *cmd, t_envlst *env)
 {
-
+	while ()
 }
 
 void	execute(t_cmd *cmd, t_envlst *env)
