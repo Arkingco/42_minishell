5dfeaa818caca2e4c4f8ee40d6e17ed6a978258b
Jaemin Jeong <jaemjeon@c1r3s6.42seoul.kr>
2022-09-12 04:52:22 +0900

    feat : doing unset_built_in#48

--- jaemjeon_jisookim_minishell/main/src/built_in/ft_unset.c
+++ jaemjeon_jisookim_minishell/main/src/built_in/ft_unset.c
@@ -6,7 +6,7 @@
 /*   By: jaemjeon <jaemjeon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 16:44:11 by jisookim          #+#    #+#             */
-/*   Updated: 2022/09/11 12:46:39 by jaemjeon         ###   ########.fr       */
+/*   Updated: 2022/09/12 04:52:04 by jaemjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,8 @@
 
 void	ft_unset(t_cmd *cmd, t_envlst *env)
 {
+	t_token *cmd_argv;
+
+	cmd_argv = cmd->simple_cmd->next;
+	while (cmd_argv != NULL)
 }
