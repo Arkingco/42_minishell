2bc1c12f55327fdc1fa3a18ad72b7fadec226112
jaemjeon <piepie1346@gmail.com>
2022-09-15 15:16:14 +0900

    feat : finish echo_builtin#48

--- jaemjeon_jisookim_minishell/main/src/built_in/ft_echo.c
+++ jaemjeon_jisookim_minishell/main/src/built_in/ft_echo.c
@@ -6,7 +6,7 @@
 /*   By: jaemjeon <jaemjeon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 16:44:01 by jisookim          #+#    #+#             */
-/*   Updated: 2022/09/15 14:02:34 by jaemjeon         ###   ########.fr       */
+/*   Updated: 2022/09/15 15:15:27 by jaemjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,17 @@ int	has_n_option(t_cmd *cmd)
 		return (FALSE);
 }
 
-void	printing_tokens(t_token *to_print_tokenlst)
+void	printing_tokens(t_token *to_print_tokenlst, int n_option_flag)
 {
 	while (to_print_tokenlst != NULL)
 	{
-
+		printf("%s ", to_print_tokenlst->string_value);
+		to_print_tokenlst = to_print_tokenlst->next;
 	}
+	if (n_option_flag == TRUE)
+		printf("\b");
+	else
+		printf("\b\n");
 }
 
 void	ft_echo(t_cmd *cmd, t_working_info *info)
@@ -36,7 +41,7 @@ void	ft_echo(t_cmd *cmd, t_working_info *info)
 	int		n_option_flag;
 	t_token	*cur_token;
 
-	cur_token = cmd->simple_cmd->next
+	cur_token = cmd->simple_cmd->next;
 	if (cur_token == NULL)
 	{
 		ft_putendl_fd("", 1);
@@ -45,7 +50,5 @@ void	ft_echo(t_cmd *cmd, t_working_info *info)
 	n_option_flag = has_n_option(cmd);
 	if (n_option_flag == TRUE)
 		cur_token = cur_token->next;
-	printing_tokens(cur_token);
-	if (n_option_flag == TRUE)
-		ft_putendl_fd("", 1);
+	printing_tokens(cur_token, n_option_flag);
 }
