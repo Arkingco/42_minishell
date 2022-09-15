e0f60a090c27cba95ebe42f4bb83b0672dcafc32
jaemjeon <piepie1346@gmail.com>
2022-09-15 14:02:53 +0900

    feat : making echo_builtin

--- jaemjeon_jisookim_minishell/main/src/built_in/ft_echo.c
+++ jaemjeon_jisookim_minishell/main/src/built_in/ft_echo.c
@@ -3,16 +3,49 @@
 /*                                                        :::      ::::::::   */
 /*   ft_echo.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jaemjeon <jaemjeon@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: jaemjeon <jaemjeon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 16:44:01 by jisookim          #+#    #+#             */
-/*   Updated: 2022/09/14 03:29:51 by jaemjeon         ###   ########.fr       */
+/*   Updated: 2022/09/15 14:02:34 by jaemjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
+int	has_n_option(t_cmd *cmd)
+{
+	char	*suspect_string;
+
+	suspect_string = cmd->simple_cmd->next->string_value;
+	if (ft_strncmp(suspect_string, "-n", 2) == 0)
+		return (TRUE);
+	else
+		return (FALSE);
+}
+
+void	printing_tokens(t_token *to_print_tokenlst)
+{
+	while (to_print_tokenlst != NULL)
+	{
+
+	}
+}
+
 void	ft_echo(t_cmd *cmd, t_working_info *info)
 {
-	printf("here is echo\n");
+	int		n_option_flag;
+	t_token	*cur_token;
+
+	cur_token = cmd->simple_cmd->next
+	if (cur_token == NULL)
+	{
+		ft_putendl_fd("", 1);
+		return ;
+	}
+	n_option_flag = has_n_option(cmd);
+	if (n_option_flag == TRUE)
+		cur_token = cur_token->next;
+	printing_tokens(cur_token);
+	if (n_option_flag == TRUE)
+		ft_putendl_fd("", 1);
 }
