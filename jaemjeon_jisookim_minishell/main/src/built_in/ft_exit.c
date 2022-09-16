2ddfee474551dda29e882a97edb333ee9922b7a8
jaemjeon <piepie1346@gmail.com>
2022-09-16 17:46:35 +0900

    feat : making exit_builtin#48

--- jaemjeon_jisookim_minishell/main/src/built_in/ft_exit.c
+++ jaemjeon_jisookim_minishell/main/src/built_in/ft_exit.c
@@ -3,15 +3,58 @@
 /*                                                        :::      ::::::::   */
 /*   ft_exit.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jaemjeon <jaemjeon@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: jaemjeon <jaemjeon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 16:44:05 by jisookim          #+#    #+#             */
-/*   Updated: 2022/09/14 03:26:48 by jaemjeon         ###   ########.fr       */
+/*   Updated: 2022/09/15 17:01:44 by jaemjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
+int	is_alnum_string(char *string)
+{
+	while (*string != '\0')
+	{
+		if (ft_isalnum(*string) == FALSE)
+			return (FALSE);
+		string++;
+	}
+	return (TRUE);
+}
+
+static void	print_error_message(char *arg_string)
+{
+	char	*result_message;
+	char	*tmp_string;
+
+	tmp_string = ft_strjoin_triple("exit: ", arg_string, ": ");
+	result_message = ft_strjoin(tmp_string, "numeric argument required");
+	free(tmp_string);
+	ft_putendl_fd(result_message, 2);
+}
+
 void	ft_exit(t_cmd *cmd, t_working_info *info)
 {
+	char	*arg_string;
+
+	ft_putendl_fd("exit", 2);
+	if (cmd->simple_cmd->next == NULL)
+		exit(0);
+	if (cmd->simple_cmd->next->next != NULL)
+	{
+		// errno를 1로 세팅해야함
+		ft_putendl_fd("exit: too many arguments", 2);
+	}
+	else
+	{
+		arg_string = cmd->simple_cmd->next->string_value;
+		if (is_alnum_string(arg_string) == FALSE)
+		{
+			print_error_message(arg_string);
+			exit(255);
+		}
+		else
+			exit(ft_atoi(arg_string));
+	}
 }
