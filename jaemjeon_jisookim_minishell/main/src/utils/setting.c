596c26a1623dd16aecc7cdc373a722f4732ceb3a
Jaemin Jeong <jaemjeon@c8r3s7.42seoul.kr>
2022-09-23 01:57:43 +0900

    feaat : added printf function by jaemjeon

--- jaemjeon_jisookim_minishell/main/src/utils/setting.c
+++ jaemjeon_jisookim_minishell/main/src/utils/setting.c
@@ -6,7 +6,7 @@
 /*   By: jaemjeon <jaemjeon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 18:37:18 by jaemjeon          #+#    #+#             */
-/*   Updated: 2022/09/23 01:35:30 by jaemjeon         ###   ########.fr       */
+/*   Updated: 2022/09/23 01:51:36 by jaemjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,29 @@ void	sigtermset(int mode)
 	set_termios(mode);
 }
 
+void	printf_fd(int fd, char *arg, ...)
+{
+
+}
+
 void	set_errno(int errno, int type)
 {
 	g_errno = errno;
-	
+	if (type == IDENTIFIER_ERR)
+	{
+		;
+	}
+	else if (type == MANY_ARG_ERR)
+	{
+		;
+	}
+	else if (type == CMD_NOT_FOUND_ERR)
+	{
+		;
+	}
+	else if (type == NOT_NUM_ARG_ERR)
+	{
+		;
+	}
 }
 
