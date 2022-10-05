a85eed5c7761454182842c5d3606f4b061bc3eb7
arkingco <arkingco@gmail.com>
2022-10-05 20:18:26 +0900

    fix : multi cmd 릭 잡기 #88 #86

--- kipark_jayoon_minishell/srcs/syntax_analysis/free_parsing_list.c
+++ kipark_jayoon_minishell/srcs/syntax_analysis/free_parsing_list.c
@@ -6,7 +6,7 @@
 /*   By: kipark <kipark@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 14:07:53 by jayoon            #+#    #+#             */
-/*   Updated: 2022/09/24 22:04:03 by kipark           ###   ########seoul.kr  */
+/*   Updated: 2022/10/05 20:17:25 by kipark           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,18 +40,26 @@ static void	free_redir_chunk_list(t_redir_chunk *head)
 
 void	free_parsing_list(t_parsing_list *l_parsing)
 {
+	t_parsing_list *next;
+
+	next = NULL;
 	if (l_parsing == NULL)
 		return ;
-	if (l_parsing->l_simple_cmd)
-		free_simple_cmd_list(l_parsing->l_simple_cmd);
-	if (l_parsing->redir_iter)
+	while (next)
 	{
-		if (l_parsing->redir_iter->l_input)
-			free_redir_chunk_list(l_parsing->redir_iter->l_input);
-		if (l_parsing->redir_iter->l_output)
-			free_redir_chunk_list(l_parsing->redir_iter->l_output);
-		free(l_parsing->redir_iter);
-	}
-	free(l_parsing);
+		next = l_parsing->next;
+		if (l_parsing->l_simple_cmd)
+			free_simple_cmd_list(l_parsing->l_simple_cmd);
+		if (l_parsing->redir_iter)
+		{
+			if (l_parsing->redir_iter->l_input)
+				free_redir_chunk_list(l_parsing->redir_iter->l_input);
+			if (l_parsing->redir_iter->l_output)
+				free_redir_chunk_list(l_parsing->redir_iter->l_output);
+			free(l_parsing->redir_iter);
+		}
+		free(l_parsing);
+	}	
 	l_parsing = NULL;
+	return ;
 }
