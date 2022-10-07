98a80e1f60553e3bad23b379a9a33df2b2fa568e
arkingco <arkingco@gmail.com>
2022-10-07 19:42:16 +0900

    fix : sleep 100 여러개 들어올 때 SIGINT 처리하기 #87

--- kipark_jayoon_minishell/includes/exit_status.h
+++ kipark_jayoon_minishell/includes/exit_status.h
@@ -3,15 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   exit_status.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jayoon <jayoon@student.42seoul.kr>         +#+  +:+       +#+        */
+/*   By: kipark <kipark@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 14:22:41 by kipark            #+#    #+#             */
-/*   Updated: 2022/10/06 16:46:48 by jayoon           ###   ########.fr       */
+/*   Updated: 2022/10/07 19:40:38 by kipark           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXIT_STATUS_H
 # define EXIT_STATUS_H
+
+# define SIGINT_EXIT 130
 
 int	g_exit_status;
 
--- kipark_jayoon_minishell/srcs/execution/wait_all_child.c
+++ kipark_jayoon_minishell/srcs/execution/wait_all_child.c
@@ -6,7 +6,7 @@
 /*   By: kipark <kipark@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/25 18:21:25 by jayoon            #+#    #+#             */
-/*   Updated: 2022/10/07 17:32:15 by kipark           ###   ########seoul.kr  */
+/*   Updated: 2022/10/07 19:41:20 by kipark           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,18 +47,22 @@ static int	init_stat_loc_after_wait(pid_t last_fork_pid, size_t num_process)
 	int		wait_pid;
 	int		stat_loc;
 	int		this_stat_loc;
+	int		is_stat_sig_int;
 
 	i = 0;
+	is_stat_sig_int = 0;
 	while (i < num_process)
 	{
 		wait_pid = wait(&stat_loc);
 		if (wait_pid != -1)
 			i++;
-		if (init_exit_status(stat_loc) == 130)
-			ft_putstr_fd("\n", 1);
+		if (init_exit_status(stat_loc) == SIGINT_EXIT)
+			is_stat_sig_int = 1;
 		if (wait_pid == last_fork_pid)
 			this_stat_loc = stat_loc;
 	}
+	if (is_stat_sig_int)
+		ft_putstr_fd("\n", 1);
 	return (this_stat_loc);
 }
 
